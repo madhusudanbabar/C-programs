052aafca30f9adac4becfad293b3e61580b844d6
madhusudan babar <madhusudanbabar@gmail.com>
2019-07-27 17:09:30 +0530

    Simplified Code

--- sort.c
+++ sort.c
@@ -1,50 +1,42 @@
 #include<stdio.h>
 int sort(int *,int );							//function to sort the array
-int ArrayIO(int *,int ,int );						//to scan and/or printthe elements in the array
 int main()
 {
+	int length,i;
 	printf("Enter the size of array : ");				//size of array
-	int length;
 	scanf("%d",&length);
 	int arr[length];
-	printf("Enter the elements now : \n");
-	ArrayIO(arr,length,1);						// 1 is for scanf
+	printf("Enter the elements now :\n");
+	for (i = 0; i < length; i++)
+	{
+		scanf("%d",&arr[i] );
+	}
 	printf("Array before sorting :\t");
-	ArrayIO(arr,length,2);						// 2 is for printf
-	sort(arr,length);						// sorting the array
-	printf("Array after sorting :\t");
-	ArrayIO(arr,length,2);						// printing sorted array
+	for (i = 0; i < length; i++)
+	{
+		printf("%d\t",arr[i] );
+	}
+	sort(&arr[0],length);						// sorting the array
+	printf("\nArray after sorting :\t");
+	for (i = 0; i < length; i++)
+	{
+		printf("%d\t",arr[i] );
+	}
 }
 
 int sort(int *arr,int length)
 {
-	for(int i=0;i<length;i++)
+	int i,j,temp;
+	for(i=0;i<length;i++)
 	{
-		for(int j=i;j<length;j++)
+		for(j=i;j<length;j++)
 		{
 			if (arr[i]>arr[j])				// swapping the elements without using another temporary variable
 			{
-				arr[i]=arr[j]+arr[i];
-				arr[j]=arr[i]-arr[j];
-				arr[i]=arr[i]-arr[j];
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
 			}
 		}
 	}
-}
-
-
-int ArrayIO(int *arr,int length,int operation)
-{
-	for(int i=0;i<length;i++)
-	{
-		if(operation==1)
-		{
-			scanf("%d",&arr[i]);
-		}
-		else
-		{
-			printf("%d\t",arr[i]);
-		}
-	}
-	printf("\n");
 }
