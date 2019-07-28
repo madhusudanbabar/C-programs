d9a233bf34d6b78af33f8d5a84b709e8a5108236
madhusudan babar <madhusudanbabar@gmail.com>
2019-07-28 17:01:14 +0530

    optimized code

--- logical_hackerrank.c
+++ logical_hackerrank.c
@@ -6,47 +6,71 @@
 
 
 void calculate_the_maximum(int n, int k) {
-    int and[n+1],or[n+1],xor[n+1],cp=0;
-    for(int a=1;a<=n;a++){
-        for(int b=1;b<a;b++){
-            if((a&b)<=k)
-            and[cp]=a&b;
-            if((a|b)<=k)
-            or[cp]=a|b;
-            if((a^b)<=k)
-            xor[cp++]=a^b;
+    int and[n+1],or[n+1],xor[n+1],cp=0,c1=0,c2=0,c3=0;
+    for(int a=1;a<=n;a++)
+    {
+        for(int b=1;b<a;b++)
+        {
+            if((a&b)<k)
+            {
+              and[c1++]=a&b;
+            }
+
+            if((a|b)<k)
+            {
+              or[c2++]=a|b;
+            }
+
+            if((a^b)<k)
+            {
+              xor[c3++]=a^b;
+            }
         }
     }
 
-    ArrayIO(and,cp);
     int andmax=0,ormax=0,xormax=0;
-    for(int i=0;i<cp;i++)
+    for(int i=0;i<c1;i++)
+    {
+        for(int j=i;j<c1;j++)
+        {
+            if (and[i]<and[j])
+            {
+                and[i]=and[j]+and[i];
+                and[j]=and[i]-and[j];
+                and[i]=and[i]-and[j];
+            }
+        }
+    }
+
+
+    for(int i=0;i<c2;i++)
+    {
+        for(int j=i;j<c2;j++)
+        {
+            if (or[i]<or[j])
+            {
+                or[i]=or[j]+or[i];
+                or[j]=or[i]-or[j];
+                or[i]=or[i]-or[j];
+            }
+        }
+    }
+
+
+
+    for(int i=0;i<c1;i++)
     {
         for(int j=i;j<cp;j++)
         {
-            if (and[i]<and[j])
-                {
-                    and[i]=and[j]+and[i];
-                    and[j]=and[i]-and[j];
-                    and[i]=and[i]-and[j];
-                }
-
-            if (or[i]<or[j])
-                {
-                    or[i]=or[j]+or[i];
-                    or[j]=or[i]-or[j];
-                    or[i]=or[i]-or[j];
-                }
-
             if (xor[i]<xor[j])
-                {
-                    xor[i]=xor[j]+xor[i];
-                    xor[j]=xor[i]-xor[j];
-                    xor[i]=xor[i]-xor[j];
-                }
+            {
+                xor[i]=xor[j]+xor[i];
+                xor[j]=xor[i]-xor[j];
+                xor[i]=xor[i]-xor[j];
+            }
         }
     }
-    printf("\n%d\n%d\n%d\n",and[cp-1],or[cp-1],xor[cp-1]);
+    printf("\n%d\n%d\n%d\n",and[0],or[0],xor[0]);
 }
 
 int main() {
