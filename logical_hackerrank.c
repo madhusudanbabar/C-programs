a2e90f80d7a180c5e2e0b828e3be1c3df9657efc
madhusudan babar <madhusudanbabar@gmail.com>
2019-07-28 17:02:35 +0530

    highly optimized code

--- logical_hackerrank.c
+++ logical_hackerrank.c
@@ -6,71 +6,29 @@
 
 
 void calculate_the_maximum(int n, int k) {
-    int and[n+1],or[n+1],xor[n+1],cp=0,c1=0,c2=0,c3=0;
+    int and[n+1],or[n+1],xor[n+1],cp=0,c1=0,c2=0,c3=0,andmax=0,ormax=0,xormax=0;
     for(int a=1;a<=n;a++)
     {
         for(int b=1;b<a;b++)
         {
-            if((a&b)<k)
+            if((a&b)<k&&(a&b)>andmax)
             {
-              and[c1++]=a&b;
+              andmax=a&b;
             }
 
-            if((a|b)<k)
+            if((a|b)<k&&(a|b)>ormax)
             {
-              or[c2++]=a|b;
+              ormax=a|b;
             }
 
-            if((a^b)<k)
+            if((a^b)<k&&(a^b)>xormax)
             {
-              xor[c3++]=a^b;
-            }
-        }
-    }
-
-    int andmax=0,ormax=0,xormax=0;
-    for(int i=0;i<c1;i++)
-    {
-        for(int j=i;j<c1;j++)
-        {
-            if (and[i]<and[j])
-            {
-                and[i]=and[j]+and[i];
-                and[j]=and[i]-and[j];
-                and[i]=and[i]-and[j];
+              xormax=a^b;
             }
         }
     }
 
-
-    for(int i=0;i<c2;i++)
-    {
-        for(int j=i;j<c2;j++)
-        {
-            if (or[i]<or[j])
-            {
-                or[i]=or[j]+or[i];
-                or[j]=or[i]-or[j];
-                or[i]=or[i]-or[j];
-            }
-        }
-    }
-
-
-
-    for(int i=0;i<c1;i++)
-    {
-        for(int j=i;j<cp;j++)
-        {
-            if (xor[i]<xor[j])
-            {
-                xor[i]=xor[j]+xor[i];
-                xor[j]=xor[i]-xor[j];
-                xor[i]=xor[i]-xor[j];
-            }
-        }
-    }
-    printf("\n%d\n%d\n%d\n",and[0],or[0],xor[0]);
+    printf("%d\n%d\n%d\n",andmax,ormax,xormax);
 }
 
 int main() {
